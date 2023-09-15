ac3d462b5e61f93c546964d9d5b4045bc408025e
viosusantoheven <101491796+viosusantoheven@users.noreply.github.com>
2023-09-15 08:32:44 +0700

    Delete Fooderlich directory

--- Fooderlich/android/app/src/main/kotlin/com/aplikasi3/aplikasi_3/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.aplikasi3.aplikasi_3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Fooderlich/lib/models/explore_data.dart
+++
@@ -1,9 +0,0 @@
-import 'models.dart';
-
-class ExploreData{
-  final List<ExploreRecipe>? todayRecipes;
-  final List<Post>? friendPosts;
-  final List<SimpleRecipe>? simpleRecipes;
-
-  ExploreData(this.todayRecipes, this.friendPosts, this.simpleRecipes);
-}
